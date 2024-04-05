f356a15a861bf90e27cfa04aadd5f937e6606005
Michael <2018118090@ub.edu.bz>
2024-04-05 15:52:25 -0600

    created most of the content I want for now

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         <a>About</a>
         <a>Contact</a>
       </nav>
-      
+
       <div>
         The meat of the website
         <div> {/*Art Post*/}
@@ -46,6 +46,94 @@ function App() {
           <p>05/04/2024</p>
           <p>Words related to Media</p>
         </div>
+      </div>
+
+      <hr/>
+
+      <div>
+        The other meat of the website
+        <div>
+          <img src={placeholder} alt="place holder image" style={{width: splashWidth}}/>
+          <span>
+            <p>Start Date</p>
+            <p>Last Worked on</p>
+            <p>Completion</p>
+          </span>
+          <p>Description of the Project</p>
+        </div>
+        <div>
+          Related Art
+          <div> {/*Art Post*/}
+          <p>Title</p>
+          <div>
+            <img src={placeholder} alt="place holder image" style={{height: mediaHeight}}/>
+          </div>
+          <p>05/04/2024</p>
+          <p>Words related to Media</p>
+        </div>
+        </div>
+      </div>
+
+      <hr/>
+
+      <div>
+        creating the meat of the website
+        <form>
+          <label>Art:</label>
+          <br/>
+          <div>
+            Art preview of art about to being uploaded
+          </div>
+          <input type="file"/>
+          <br/>
+          <label>Type:</label>
+          <select>
+            <option>Normal</option>
+            <option>Project</option>
+          </select>
+          <br/>
+          <label>Title:</label>
+          <input type="text"/>
+          <br/>
+          <label>Date:</label>
+          <input type="date"/>
+          <br/>
+          <label>Words:</label>
+          <textarea/>
+          <br/>
+          <button type="submit">Create </button>
+        </form>
+      </div>
+      
+      <hr/>
+
+      <div>
+        creating the other meat of the website
+        <form>
+          <label>Art:</label>
+          <br/>
+          <div>
+            Art preview of art about to being uploaded
+          </div>
+          <input type="file"/>
+          <br/>
+          <label>Type:</label>
+          <select>
+            <option>Exploratory</option>
+            <option>Serious</option>
+          </select>
+          <br/>
+          <label>Project Name:</label>
+          <input type="text"/>
+          <br/>
+          <label>Start Date:</label>
+          <input type="date"/>
+          <br/>
+          <label>Description:</label>
+          <textarea/>
+          <br/>
+          <button type="submit">Create </button>
+        </form>
       </div>
     </>
   );
