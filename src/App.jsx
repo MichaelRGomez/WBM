bb942f2136e4a1d35e100002418c8e0f34f92159
Michael <2018118090@ub.edu.bz>
2024-04-05 14:21:33 -0600

    figuring out what website to make

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,19 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <h1>Website header text</h1>
+      <nav>
+        <a>Home Link</a>
+        <a>The actual content</a>
+        <a>About</a>
+        <a>Contact</a>
+      </nav>
+      <div>
+        The meat of the website
+        <div>
+          context that was definitely fetched from a database
+        </div>
+      </div>
     </>
   );
 };
