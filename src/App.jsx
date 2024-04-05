aac8f10e7836485a435ac74122eeab45f1f5c3d2
Michael <2018118090@ub.edu.bz>
2024-04-05 15:37:58 -0600

    saving just in case

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,49 @@
 import './App.css'
-import placeholder from '../public/placeholder.png'
+import placeholder from '/placeholder.png'
+import world from '/world.gif'
+import funny from '/funny.mp4'
 
 function App() {
+  const mediaHeight = "400px";
+  const splashWidth = "900px";
+
   return (
     <>
       <h1>Website header text</h1>
+
       <nav>
         <a>Home Link</a>
         <a>The actual content</a>
         <a>About</a>
         <a>Contact</a>
       </nav>
+      
       <div>
         The meat of the website
         <div> {/*Art Post*/}
           <p>Title</p>
           <div>
-            <img src={placeholder} alt="place holder image" style={{height: "200px"}}/>
+            <img src={placeholder} alt="place holder image" style={{height: mediaHeight}}/>
           </div>
+          <p>05/04/2024</p>
           <p>Words related to Media</p>
         </div>
         <div> {/*Art Post*/}
           <p>Title</p>
-          <div>Media</div>
+          <div>
+            <img src={world} alt="the world spinning" style={{height: mediaHeight}}/>
+          </div>
+          <p>05/04/2024</p>
           <p>Words related to Media</p>
         </div>
         <div> {/*Art Post*/}
           <p>Title</p>
-          <div>Media</div>
+          <div>
+            <video style={{height: mediaHeight}} controls>
+              <source type="video/mp4" src={funny}/>
+            </video>
+          </div>
+          <p>05/04/2024</p>
           <p>Words related to Media</p>
         </div>
       </div>
