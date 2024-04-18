5a6a3905bace95b82e0e41d9c4caa2bf485abcc7
Michael <2018118090@ub.edu.bz>
2024-04-18 10:42:38 -0600

    starting over from scratch

--- src/main.jsx
+++ src/main.jsx
@@ -5,47 +5,8 @@ import {
   RouterProvider
 } from "react-router-dom";
 import './index.css';
-import Root from './routes/Root.jsx';
-import ErrorPage from './pages/ErrorPage.jsx';
-import Posts from './pages/Posts.jsx';
-import Projects from './pages/Projects.jsx';
-import CreateArt from './pages/CreateArt.jsx';
-import CreateProject from './pages/CreateProject.jsx';
-import Home from './pages/Home.jsx'
-
-const mediaHeight = "400px";
-const splashWidth = "900px";
-const miniSplashHeight = "120px";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "Home",
-        element: <Home mediaHeight={mediaHeight}/>,
-      },
-      {
-        path: "Posts",
-        element: <Posts  mediaHeight={mediaHeight} splashWidth={splashWidth}/>    
-      },
-      {
-        path: "Project/List",
-        element: <Projects miniSplashHeight={miniSplashHeight}/>
-      },
-      {
-        path: "Post/Create",
-        element: <CreateArt />
-      },
-      {
-        path: "Project/Create",
-        element: <CreateProject />
-      }
-    
-    ]
-  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
